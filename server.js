6439479f0b93223c41d233af21fad316df8c1d7d
Shehryar Ehtesham <se318@rutgers.edu>
2020-01-24 11:05:02 -0500

    redesigning server js

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
+//Requiring express 
 var express = require("express");
+
+//Requiring the models folder 
+var db = require("./models");
+
+//declaring port to 8090
 var PORT = process.env.PORT || 8090;
+
+//initializing express 
 var app = express();
+
+
+//Middlewear Configuration 
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+//Making public folder static
 app.use(express.static("public"));
 // app.get("/", function(req, res) {
 // });
+
+
+
+
 
 
 
